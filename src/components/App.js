6a044a79e4ee0ce68309de68c2b1cb7621ada2fe
jamezjaz <jamezjaz@gmail.com>
2021-03-15 11:20:22 -0500

    docs: Installs PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Book Ticket</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Book Ticket</h1>
+  </div>
+);
 
 export default App;
