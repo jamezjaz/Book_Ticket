28f8e6fa2d36f6e10f651047239dd07fb25a5909
jamezjaz <jamezjaz@gmail.com>
2021-03-17 08:16:59 -0500

    docs: Implements handleChange function

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => (
-  <div>
-    <form>
-      <label htmlFor="email">
-        Email:
-        <input type="text" name="email" id="email" placeholder="Enter your email" />
-      </label>
-      <br />
-      <label htmlFor="password">
-        Password:
-        <input type="password" name="password" id="password" placeholder="Enter your password" />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
-  </div>
-);
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [error, setError] = useState('');
+
+  const handleChange = event => {
+    if (event.target.name === 'email') {
+      setEmail(event.target.value);
+    } else {
+      setPassword(event.target.value);
+    }
+    console.log(event.target);
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input type="text" name="email" id="email" placeholder="Enter your email" onChange={handleChange} value={email.email} />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input type="password" name="password" id="password" placeholder="Enter your password" onChange={handleChange} value={password.password} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
