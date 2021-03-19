95caa63179ee25ae1334deb6bec80bd9adc1628a
jamezjaz <jamezjaz@gmail.com>
2021-03-19 12:10:21 -0500

    docs: Installs react-bootstrap bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
