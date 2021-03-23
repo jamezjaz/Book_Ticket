76993bfa260715533fc232685ff26521fd922e58
jamezjaz <jamezjaz@gmail.com>
2021-03-23 15:40:14 -0500

    docs: Imports sideNav component on AirlineDetails component

--- src/container/AirlineDetails.js
+++ src/container/AirlineDetails.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AirlineDetailsStyles from '../styles/AirlineDetailsStyles.module.css';
+import SideNav from '../components/SideNav';
 
 const AirlineDetails = ({ airlines, match }) => {
   const airlineId = parseInt(match.params.id, 10);
 
   return (
-    <div className="container-fluid">
-      {airlines.filter(airline => airline.id === airlineId).map(airline => (
-        <div key={airline.id} className={AirlineDetailsStyles.details}>
-          <img src={airline.image.url} className={AirlineDetailsStyles.img} alt={airline.name} />
-          <div className={AirlineDetailsStyles.data}>
-            <h4>{airline.name}</h4>
-            <p>{`$ ${airline.price}`}</p>
-            <span>{airline.description}</span>
-            <p>{airline.location}</p>
+    <div className="d-flex container-fluid">
+      <div className="col-2 d-none d-md-flex border px-0">
+        <SideNav />
+      </div>
+      <div className="col-10 px-0">
+        {airlines.filter(airline => airline.id === airlineId).map(airline => (
+          <div key={airline.id} className={AirlineDetailsStyles.details}>
+            <img src={airline.image.url} className={AirlineDetailsStyles.img} alt={airline.name} />
+            <div className={AirlineDetailsStyles.data}>
+              <h4>{airline.name}</h4>
+              <p>{`$ ${airline.price}`}</p>
+              <span>{airline.description}</span>
+              <p>{airline.location}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
