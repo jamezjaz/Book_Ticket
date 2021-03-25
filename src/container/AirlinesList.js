ffe348ba6baa2641d6e25fc42d9fb33b843d0b28
jamezjaz <jamezjaz@gmail.com>
2021-03-25 13:19:46 -0500

    docs: Installs react-loader-spinner
    docs: Adds loader spinner to curb the waiting effect of the asyn call

--- src/container/AirlinesList.js
+++ src/container/AirlinesList.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import { Dropdown } from 'react-bootstrap';
+import Loader from 'react-loader-spinner';
 import fetchAirlines from '../apiRequests/getAirlineRequest';
 import Airline from '../components/Airline';
 import responsive from '../constants/respCarousel';
 import '../styles/AliceCarousel.css';
 import SideNav from '../components/SideNav';
 import menu from '../assets/hamburger-menu.png';
+import AirlinesListStyles from '../styles/AirlinesListStyles.module.css';
 
 const AirlinesList = props => {
+  const [loading, setLoading] = useState(true);
   const { airlines } = props;
   useEffect(() => {
     const { fetchedAirlines } = props;
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
     fetchedAirlines(airlines);
   }, []);
 
@@ -23,35 +29,48 @@ const AirlinesList = props => {
       <div className="col-2 d-none d-md-flex border px-0">
         <SideNav />
       </div>
-      <div className="col-10 px-0">
-        <Dropdown className="d-flex d-md-none mb-5">
-          <Dropdown.Toggle className="bg-white" id="dropdown-basic">
-            <img src={menu} alt="menu" />
-          </Dropdown.Toggle>
+      {loading === true
+        ? (
+          <Loader
+            type="Rings"
+            color="#97BF0F"
+            height={150}
+            width={150}
+            timeout={6000}
+            className={AirlinesListStyles.spinner}
+          />
+        )
+        : (
+          <div className="col-10 px-0">
+            <Dropdown className="d-flex d-md-none mb-5">
+              <Dropdown.Toggle className="bg-white" id="dropdown-basic">
+                <img src={menu} alt="menu" />
+              </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="/airlinesList" active>AIRLINES</Dropdown.Item>
-            <Dropdown.Item href="/tickets">TICKETS</Dropdown.Item>
-            <Dropdown.Item href="/others">OTHERS</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <h1 className="mt-5">Trending Airlines</h1>
-        <AliceCarousel
-          responsive={responsive}
-          autoPlayInterval={3200}
-          autoPlayDirection="ltr"
-          autoPlay
-          fadeOutAnimation
-          mouseTrackingEnabled
-          disableAutoPlayOnAction
-          dotsDisabled
-          infinite
-        >
-          {airlines.map(airline => (
-            <Airline key={airline.id} airline={airline} />
-          ))}
-        </AliceCarousel>
-      </div>
+              <Dropdown.Menu>
+                <Dropdown.Item href="/airlinesList" active>AIRLINES</Dropdown.Item>
+                <Dropdown.Item href="/tickets">TICKETS</Dropdown.Item>
+                <Dropdown.Item href="/others">OTHERS</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <h1 className="mt-5">Trending Airlines</h1>
+            <AliceCarousel
+              responsive={responsive}
+              autoPlayInterval={3200}
+              autoPlayDirection="ltr"
+              autoPlay
+              fadeOutAnimation
+              mouseTrackingEnabled
+              disableAutoPlayOnAction
+              dotsDisabled
+              infinite
+            >
+              {airlines.map(airline => (
+                <Airline key={airline.id} airline={airline} />
+              ))}
+            </AliceCarousel>
+          </div>
+        )}
     </div>
   );
 };
