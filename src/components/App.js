783bd65f818ce19b3976de609b30245c5a1f9a4a
jamezjaz <jamezjaz@gmail.com>
2021-03-29 18:54:52 -0500

    style: Styles Home component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import Routes from '../Routes';
 import '../styles/App.css';
 
 const App = () => (
-  <div className="App py-3">
+  <div className="App">
     <Routes />
   </div>
 );
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HomeStyles from '../styles/HomeStyles.module.css';
 
 const Home = () => (
-  <div>
-    <Link to="/login">
-      <button type="button">Login</button>
-    </Link>
-    <Link to="/register">
-      <button type="button">Register</button>
-    </Link>
+  <div className={HomeStyles.home}>
+    <h1 className={HomeStyles.heading}>BOOKING AIR TICKETS MADE EASY</h1>
+    <div>
+      <Link to="/login" className={HomeStyles.leftBtn}>
+        <button type="button" className={`${HomeStyles.btn} btn`}>Login</button>
+      </Link>
+      <Link to="/register" className={HomeStyles.rightBtn}>
+        <button type="button" className={`${HomeStyles.btn} btn`}>Register</button>
+      </Link>
+    </div>
   </div>
 );
 
